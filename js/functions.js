0867fad0d6f903d5aeb6bc7dfac74f11a1063645
SKAT6130 <140112937+SKAT6130@users.noreply.github.com>
2024-03-02 04:30:10 +0400

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,9 @@
 // 1-ое задание (Проверка на количество символов)
 function result(string, maxSymbols) {
   //Добавляем условие через тернарный оператор
-  return string.length <= maxSymbols ? "true" : "false";
+  return string.length <= maxSymbols ? 'true' : 'false';
 }
 
-result("проверяемая строка", 20);
-result("проверяемая строка", 18);
-result("проверяемая строка", 10);
+result('проверяемая строка', 20);
+result('проверяемая строка', 18);
+result('проверяемая строка', 10);
