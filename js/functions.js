0bd6406ee8a0b5ef3954da6d322a922f78fa5996
Andrey <skat6130@yandex.ru>
2024-03-03 05:16:03 +0400

    Делу-время

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,24 @@ function result(string, maxSymbols) {
 result('проверяемая строка', 20);
 result('проверяемая строка', 18);
 result('проверяемая строка', 10);
+
+// Делу - время
+function checkMeetingTime(startWork, endWork, startMeeting, meetingDuration) {
+
+  // Преобразуем принятое время в объект Date
+  const workStart = new Date('2024-03-03 ${startWork}');
+  const workEnd = new Date('2024-03-03 ${endWork}');
+  const meetingStart = new Date('2024-03-03 ${startMeeting}');
+
+  // Прибавим продолжительность встречи к времени начала встречи
+  meetingStart.setMinutes(meetingStart.getMinutes() + meetingDuration);
+
+  // Проверяем попадает ли время встречи в рабочее время
+  return meetingStart >= workStart && meetingStart <= workEnd;
+}
+
+checkMeetingTime('08:00', '17:30', '14:00', 90);
+checkMeetingTime('8:0', '10:0', '8:0', 120);
+checkMeetingTime('08:00', '14:30', '14:00', 90);
+checkMeetingTime('14:00', '17:30', '08:0', 90);
+checkMeetingTime('8:00', '17:30', '08:00', 900);
