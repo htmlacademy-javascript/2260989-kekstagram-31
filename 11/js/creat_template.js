0749ea314a0c76bf65479ee8e001529bf8ed15ff
Keks <keks@htmlacademy.ru>
2024-03-21 18:07:20 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/creat_template.js
@@ -0,0 +1,28 @@
+import { photos } from './data.js';
+import { onPictureOpen } from './big-picture.js';
+
+const pictures = document.querySelector('.pictures'); // Нашли блок pictures в который будут отрисованы элементы
+const templatePicture = document.querySelector('#picture').content.querySelector('.picture'); // Нашли шаблон
+
+const pictureListFragment = document.createDocumentFragment(); // Создаем пустой контейнер
+
+// Функция создания DOM-элементов и заполнения их данными
+const createTemplate = (photo) => {
+  const photoElement = templatePicture.cloneNode(true); //Делаем копию шаблона
+  photoElement.querySelector('.picture__img').src = photo.url;
+  photoElement.querySelector('.picture__img').alt = photo.description;
+  photoElement.querySelector('.picture__likes').textContent = photo.likes;
+  photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
+
+  onPictureOpen(photoElement, photo);
+  return photoElement;
+};
+
+const createTemplates = () => {
+  photos.forEach((photo) => {
+    pictureListFragment.appendChild(createTemplate(photo));
+  });
+  pictures.appendChild(pictureListFragment);
+};
+
+createTemplates();
---
+++ 11/js/main.js
@@ -0,0 +1,5 @@
+import './creat_template.js';
+import './data.js';
+import './big-picture.js';
+import './upload-img.js';
+import './scale.js';
