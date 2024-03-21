f00a95f57b2ae0eea4706cfbd01ed1d1a57f45d5
SKAT6130 <140112937+SKAT6130@users.noreply.github.com>
2024-03-21 18:31:50 +0400

    Update scale.js

--- js/scale.js
+++ js/scale.js
@@ -3,15 +3,15 @@ const MIN_SCALE = 25;
 const MAX_SCALE = 100;
 const DEFAULT_SCALE = 100;
 
-const modalElement = document.querySelector(".img-upload");
+const modalElement = document.querySelector('.img-upload');
 const smallerButtonElement = modalElement.querySelector(
-  ".scale__control--smaller",
+  '.scale__control--smaller',
 );
 const biggerButtonElement = modalElement.querySelector(
-  ".scale__control--bigger",
+  '.scale__control--bigger',
 );
-const scaleInputElement = modalElement.querySelector(".scale__control--value");
-const imageElement = modalElement.querySelector(".img-upload__preview img");
+const scaleInputElement = modalElement.querySelector('.scale__control--value');
+const imageElement = modalElement.querySelector('.img-upload__preview img');
 
 const scaleImage = (value) => {
   imageElement.style.transform = `scale(${value / 100})`;
@@ -32,7 +32,7 @@ const onBiggerButtonClick = () => {
 
 const resetScale = () => scaleImage(DEFAULT_SCALE);
 
-smallerButtonElement.addEventListener("click", onSmallerButtonClick);
-biggerButtonElement.addEventListener("click", onBiggerButtonClick);
+smallerButtonElement.addEventListener('click', onSmallerButtonClick);
+biggerButtonElement.addEventListener('click', onBiggerButtonClick);
 
 export { resetScale };
