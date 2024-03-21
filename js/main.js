fc51db1e0e05522899c5307c83384b18ae8de6d9
SKAT6130 <140112937+SKAT6130@users.noreply.github.com>
2024-03-21 18:26:03 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import "./creat_template.js";
-import "./data.js";
-import "./big-picture.js";
-import "./upload-img.js";
-import "./scale.js";
+import './creat_template.js';
+import './data.js';
+import './big-picture.js';
+import './upload-img.js';
+import './scale.js';
