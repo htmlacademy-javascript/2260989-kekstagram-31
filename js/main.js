2b72629f2f23a634aa95c5b1ad3537192d05bb91
SKAT6130 <140112937+SKAT6130@users.noreply.github.com>
2024-04-04 14:54:37 +0400

    Recent improvements

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-import './creat_template.js';
-import './data.js';
-import './big-picture.js';
 import './upload-img.js';
-import './scale.js';
+import { showErrorMessage } from './util.js';
+import { loadPicture } from './api.js';
+import { renderPhoto } from './photos.js';
+import { initFilter } from './filters.js';
+
+async function bootstrap() {
+  try {
+    const pictures = await loadPicture();
+    renderPhoto(pictures);
+    initFilter(pictures);
+  } catch {
+    showErrorMessage();
+  }
+}
+
+bootstrap();
